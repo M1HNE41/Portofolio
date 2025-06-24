c97946200ab169865b9a01f7a6871e335e973764
Petcu ‎Mihnea <mihnea1009@gmail.com>
2025-06-24 15:09:57 +0300

    First commit

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Mihnea Petcu",
+  DESCRIPTION: "Hi, I’m Mihnea Petcu. This is my portfolio, where code meets curiosity and cybersecurity.",
+  AUTHOR: "Mihnea Petcu",
 }
 
 // Work Page
@@ -56,26 +56,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "mihnea1009@gmail.com",
+    HREF: "mailto:mihnea1009@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "M1HNE41",
+    HREF: "https://github.com/M1HNE41"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "Mihnea Petcu",
+    HREF: "https://www.linkedin.com/in/mihnea-petcu-458467216/",
   },
 ]
 
